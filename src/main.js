04eea708c1b263866a439987b69e627bf499a9ca
Patrick-van-Halm <pvanhalm00@gmail.com>
2021-03-09 11:17:00 +0100

    Change reviews page

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import screen from 'vue-screen';
 
-Vue.use(screen);
+Vue.use(screen, {
+  xs: 600,
+  sm: 960,
+  md: 1264,
+  lg: 1904,
+});
 
 Vue.config.productionTip = false
 
--- src/views/Reviews.vue
+++ src/views/Reviews.vue
@@ -6,10 +6,12 @@
           <h2 class="fs-2 dancing-script">All my reviews</h2>
         </v-col>
       </v-row>
-      <v-row >
-        <v-col class="d-flex justify-content-center flex-column pl-0" cols="1">
-          <v-btn v-for="letter in alphabet" v-bind:key="letter" tile class="px-0" :class="{ active: (selected_letter == letter || selected_letter == '' && letter == 'all') }" min-width="36" @click="setLetterFilter(letter)">{{letter}}</v-btn>
+      <v-row>
+        <v-col class="text-center">
+          <v-btn v-for="letter in alphabet" v-bind:key="letter" tile class="px-2 my-1 ml-1 bg-tertiary" :class="{ active: (selected_letter == letter || selected_letter == '' && letter == 'all') }" min-width="36" @click="setLetterFilter(letter)">{{letter}}</v-btn>
         </v-col>
+      </v-row>
+      <v-row>
         <v-col>
           <v-list flat class="py-0" v-if="!selected_letter && !selected_author">
             <v-subheader class="pa-0">
@@ -18,8 +20,8 @@
                   <v-col class="d-flex flex-column justify-center">
                     <h3 class="black--text fs-1_5">All books</h3>
                   </v-col>
-                  <v-col cols="2">
-                    <v-text-field label="Filter" v-model="filterValue" @input="OnFilterChange()"/>
+                  <v-col cols="5" md="4" lg="3">
+                    <v-text-field label="Search" v-model="filterValue" @input="OnFilterChange()"/>
                   </v-col>
                 </v-row>
               </v-container>
@@ -34,16 +36,19 @@
           </v-list>
           <v-list flat class="py-0" v-else-if="selected_letter && !selected_author">
             <v-subheader class="pa-0"><h3 class="black--text fs-1_5">Authors starting with letter:<span class="ml-1 text-uppercase">{{selected_letter}}</span></h3></v-subheader>
-            <v-list-item-group>
-              <v-list-item
-                v-for="(author, i) in filteredAuthors"
-                :key="i"
-              >
-                <v-list-item-content>
-                  <v-list-item-title v-text="author" @click="setAuthorFilter(author)"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
+            <v-container>
+              <v-row class="flex-column" v-for="(author, i) in filteredAuthors" :key="i">
+                <v-col cols="12" offset="0" md="4" offset-md="4" class="pa-1">
+                  <v-btn
+                    block
+                    @click="setAuthorFilter(author)"
+                    class="bg-tertiary"
+                  >
+                  {{author}}
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
           </v-list>
           <v-list flat class="py-0" v-else-if="selected_author">
             <v-subheader class="pa-0 black--text fs-1_5"><h3 class="black--text fs-1_5">Books by: {{selected_author}}</h3></v-subheader>
@@ -64,7 +69,7 @@
 <style>
   .active{
     font-weight: 800;
-    background-color: #cdcaca !important;
+    background-color: #6e98ae !important;
   }
 </style>
 
